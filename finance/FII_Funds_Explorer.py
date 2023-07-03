6080412e2d0ebfe433980d879ebce44ab0c46dea
Robertson Pinto Dias Jr <robertson.diasjr@gmail.com>
2023-07-03 11:03:55 -0300

    updates

--- finance/FII_Funds_Explorer.py
+++ finance/FII_Funds_Explorer.py
@@ -6,6 +6,8 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import pandas as pd
+from unidecode import unidecode
+import sqlite3
 
 # Set up Selenium Chrome driver
 chrome_options = Options()
@@ -22,23 +24,54 @@ table_xpath = '//*[@id="upTo--default-fiis-table"]/div/table'
 table_element = table = driver.find_element(By.XPATH, table_xpath)
 
 # Get the HTML content of the table
-table_html = table_element.get_attribute('outerHTML')
+table_html = table_element.get_attribute("outerHTML")
 
 # Close the Selenium webdriver
 driver.quit()
 
 # Read the HTML table into a Pandas DataFrame
-table = df = pd.read_html(table_html, encoding='utf-8', thousands='.')[0]
+table = table = pd.read_html(table_html, encoding='utf-8', thousands='.')[0]
 
-# Remove column A and B from table A
-table = table.drop(['DY (3M) Acumulado', 'DY (6M) Acumulado', 'DY (3M) média', 'DY (6M) média'], axis=1)
+# Column normalization
 
-# Display on screen
-#print (table)
+# Replace R$ with BRL (Brazilian currency)
+table.rename(columns=lambda x: x.replace('R$', ''), inplace=True)
 
-# Export merged table to CSV or Excel
-#table.to_csv('FII_Funds_Explorer.csv', index=False)
+# Replace whitespace with underscore
+table.rename(columns=lambda x: x.replace(' ', '_'), inplace=True)
+
+# Remove underscore in the end of strings
+table.columns = table.columns.str.rstrip('_')
+
+# Remove .
+table.rename(columns=lambda x: x.replace('.', ''), inplace=True)
+
+# Remove parentheses from column names using lambda and rename
+table.rename(columns=lambda x: x.replace('(', ''), inplace=True)
+table.rename(columns=lambda x: x.replace(')', ''), inplace=True)
+
+# Uppercase all the names
+table.columns = table.columns.str.upper()
+
+# Remove accents
+table.columns = [unidecode(col) for col in table.columns]
+
+# SQLite function
+def export_to_sqlite():
+    # Create a connection to the SQLite database
+    conn = sqlite3.connect('fii.db')
+
+    # Convert the pandas DataFrame into a SQLite table
+    table.to_sql('fii', conn, if_exists='replace', index=False)
+
+    # Close the database connection
+    conn.close()
+
+# Export to SQLite
+# export_to_sqlite()
+
+# Export table to CSV
+# table.to_csv('FII_Funds_Explorer.csv', index=False)
+
+# Export table to Excel
 table.to_excel('FII_Funds_Explorer.xlsx', index=False)
-
-print ('Success')
-
