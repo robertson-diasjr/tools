4858f31b29c523b004fbe76635664fec2086aa16
Robertson Pinto Dias Jr <77121376+robertson-diasjr@users.noreply.github.com>
2023-08-18 17:33:02 -0300

    Update FII_Clube_FII.py

--- finance/FII_Clube_FII.py
+++ finance/FII_Clube_FII.py
@@ -12,6 +12,7 @@ import matplotlib.pyplot as plt
 
 # Set up Selenium 4 Chrome driver
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
